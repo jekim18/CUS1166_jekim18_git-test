e93260bf080649368b85468b5b05318eb0642b96
jekim18 <54969646+jekim18@users.noreply.github.com>
2019-10-08 15:20:19 -0400

    Remove commented code

--- cus1166_lab05/models.py
+++ cus1166_lab05/models.py
@@ -27,30 +27,3 @@ class Link(db.Model):
        db.Integer,
        db.ForeignKey('course.id'),
        primary_key=True)
-
-'''
-class Course(db.Model):
-    __tablename__ = "courses"
-    id = db.Column(db.Integer, primary_key=True)
-    course_number = db.Column(db.String, nullable=False)
-    course_title = db.Column(db.String, nullable=False)
-    students = db.relationship('RegisteredStudent', secondary='studentcourses')
-
-class RegisteredStudent(db.Model):
-    __tablename__ = "registeredstudents"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    grade = db.Column(db.Integer, nullable=False)
-    courses = db.relationship('Course', secondary='studentcourses')
-
-class StudentCourses(db.Model):
-    __tablename__ = 'studentcourses'
-    student_id = db.Column(
-        db.Integer,
-        db.ForeignKey('registeredstudents.id'),
-        primary_key=True)
-    course_id = db.Column(
-        db.Integer,
-        db.ForeignKey('courses.id'),
-        primary_key=True)
-'''
